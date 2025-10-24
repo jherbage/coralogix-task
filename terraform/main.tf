06dfe556bfaab24238bab08a24466ecc04a3347f
Jason Herbage <jaherbag@cisco.com>
2025-10-24 08:38:46 +0100

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,47 @@ resource "coralogix_rules_group" "fraud_logs" {
   }
 }
 
+resource "coralogix_rules_group" "block_otel_value_logs" {
+  name         = "block the values explosion"
+  description  = ""
+  applications = []
+  subsystems   = ["coralogix-opentelemetry-collector","coralogix-opentelemetry-agent"]
+  severities   = []
+  order        = 1
+
+  rule_subgroups {
+    rules {
+      block {
+        name               = "Block values expressions"
+        source_field       = "text"
+        regular_expression = "one or more paths were modified to include their context prefix, please rewrite them accordingly"
+      }
+    }
+  }
+}
+
+resource "coralogix_rules_group" "fix_payment_service_mapping" {
+  name         = "Fix Mapping Issue Payment Service"
+  description  = ""
+  applications = []
+  subsystems   = ["payment"]
+  severities   = []
+  order        = 1
+
+  rule_subgroups {
+    rules {
+      replace {
+        name               = "Replace service.name with service_name"
+        description        = "{\"loyalty_level\":\"platinum\",\"hostname\":\"payment-d6b878974-gvqgk\",\"trace_id\":\"d77c7f1c88a204db001df5b7b86058ca\",\"scope\":{\"attributes\":{}},\"observedTimeUnixNano\":1757419380201402170,\"lastFourDigits\":\"1278\",\"trace_flags\":\"01\",\"pid\":17,\"resource\":{\"attributes\":{\"k8s.container.name\":\"payment\",\"k8s.namespace.name\":\"default\",\"k8s.pod.name\":\"payment-d6b878974-gvqgk\",\"k8s.container.restart_count\":\"0\",\"k8s.pod.uid\":\"988a9fd1-c49f-40b2-a168-07cf4817b0bf\",\"k8s.cluster.name\":\"se-demo\",\"cx.otel_integration.name\":\"coralogix-integration-helm\",\"host.name\":\"minikube\",\"os.type\":\"linux\",\"host.id\":\"8cea92daa17d43978a2c10bb0f33194e\",\"k8s.node.name\":\"minikube\",\"k8s.deployment.name\":\"payment\"}},\"span_id\":\"1be1ac6e7f5758b8\",\"msg\":\"Transaction complete.\",\"time\":1757419380046,\"service.name\":\"payment\",\"level\":\"info\",\"attributes\":{\"log.iostream\":\"stdout\",\"log.file.path\":\"/var/log/pods/default_payment-d6b878974-gvqgk_988a9fd1-c49f-40b2-a168-07cf4817b0bf/payment/0.log\",\"time\":\"2025-09-09T12:03:00.04810967Z\"},\"amount\":{\"units\":{\"low\":90,\"high\":0,\"unsigned\":false},\"nanos\":289999999,\"currencyCode\":\"USD\"},\"resourceSchemaUrl\":\"https://opentelemetry.io/schemas/1.6.1\",\"cardType\":\"visa\",\"timeUnixNano\":1757419380048109670,\"transactionId\":\"1e1ee7cc-555c-4e4e-8d92-181daa13bd04\"}"
+        source_field       = "text"
+        destination_field  = "text"
+        regular_expression = "service.name\":"
+        replacement_string = "service_name\":"
+      }
+    }
+  }
+}
+
 resource "coralogix_events2metric" "logs2metric_example" {
   name        = "payment_example"
   description = ""
